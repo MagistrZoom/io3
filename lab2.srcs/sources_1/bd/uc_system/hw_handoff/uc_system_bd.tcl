558bf5db0c0598679f25b406cf3a7c639bc2ae69
Vladimir Gubarev <vladimir.gubarev@itiviti.com>
2018-01-14 21:04:51 +0300

    Build working version

--- lab2.srcs/sources_1/bd/uc_system/hw_handoff/uc_system_bd.tcl
+++ lab2.srcs/sources_1/bd/uc_system/hw_handoff/uc_system_bd.tcl
@@ -325,7 +325,7 @@ CONFIG.C_GPIO_WIDTH {16} \
   # Create instance: axi_uartlite_0, and set properties
   set axi_uartlite_0 [ create_bd_cell -type ip -vlnv xilinx.com:ip:axi_uartlite:2.0 axi_uartlite_0 ]
   set_property -dict [ list \
-CONFIG.C_BAUDRATE {9600} \
+CONFIG.C_BAUDRATE {115200} \
  ] $axi_uartlite_0
 
   # Create instance: clk_wiz_0, and set properties
